fbd0e0978126591a7629d0a1e3eaaf32ed3503e5
Unknown <dima.krasovsky@outlook.com>
2018-09-20 15:53:59 +0300

    [DEV]: Added navigation component

--- main/src/main/java/com/krasovsky/dima/demoproject/main/view/activity/MenuActivity.kt
+++ main/src/main/java/com/krasovsky/dima/demoproject/main/view/activity/MenuActivity.kt
@@ -2,12 +2,24 @@ package com.krasovsky.dima.demoproject.main.view.activity
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.NavigationView
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.NavHostFragment.findNavController
+import androidx.navigation.ui.NavigationUI.setupWithNavController
 import com.krasovsky.dima.demoproject.main.R
+import kotlinx.android.synthetic.main.activity_menu.*
 
 class MenuActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
+
+        initView()
+    }
+
+    private fun initView() {
+        setupWithNavController(bottom_navigation, findNavController(my_nav_host_fragment))
     }
 }
--- main/src/main/java/com/krasovsky/dima/demoproject/main/view/activity/SplashActivity.kt
+++ main/src/main/java/com/krasovsky/dima/demoproject/main/view/activity/SplashActivity.kt
@@ -2,12 +2,30 @@ package com.krasovsky.dima.demoproject.main.view.activity
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.SystemClock
 import com.krasovsky.dima.demoproject.main.R
+import kotlinx.coroutines.experimental.android.UI
+import kotlinx.coroutines.experimental.async
+import kotlinx.coroutines.experimental.delay
+import kotlinx.coroutines.experimental.launch
+import org.jetbrains.anko.startActivity
 
 class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        startHandler()
+    }
+
+    private fun startHandler() {
+        launch(UI) {
+            val result = async { delay(3000) }.await()
+
+            startActivity<MenuActivity>()
+            SplashActivity@finish()
+        }
+
     }
 }
