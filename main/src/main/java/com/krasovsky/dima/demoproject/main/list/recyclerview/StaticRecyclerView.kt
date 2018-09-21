b11671752fd3cb2aa2760c3b1d9e12c81f64291a
Unknown <dima.krasovsky@outlook.com>
2018-09-21 12:30:07 +0300

    [DEV]: Added static recyclerView

---
+++ main/src/main/java/com/krasovsky/dima/demoproject/main/list/recyclerview/StaticRecyclerView.kt
@@ -0,0 +1,27 @@
+package com.krasovsky.dima.demoproject.main.list.recyclerview
+
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.view.ViewGroup
+
+class StaticRecyclerView : RecyclerView.Adapter<StaticRecyclerView.StaticVH>() {
+
+
+    override fun onCreateViewHolder(p0: ViewGroup, p1: Int): StaticVH {
+
+    }
+
+    override fun getItemCount(): Int {
+
+    }
+
+    override fun onBindViewHolder(p0: StaticVH, p1: Int) {
+
+    }
+
+
+    inner class StaticVH(view: View) : RecyclerView.ViewHolder(view) {
+
+    }
+
+}
