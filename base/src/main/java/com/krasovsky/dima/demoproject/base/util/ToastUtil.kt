97e260557bece58e4ec5642f07192a9d4af84693
Dima <dima.krasovsky@outlook.com>
2018-11-18 19:40:06 +0300

    [DEV]: Changed gravity of toasts

--- base/src/main/java/com/krasovsky/dima/demoproject/base/util/ToastUtil.kt
+++ base/src/main/java/com/krasovsky/dima/demoproject/base/util/ToastUtil.kt
@@ -8,6 +8,7 @@ import com.krasovsky.dima.demoproject.base.R
 import org.jetbrains.anko.margin
 import org.jetbrains.anko.padding
 import android.support.v4.content.ContextCompat.getSystemService
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 
@@ -18,6 +19,8 @@ fun Context.getPositiveToast(message: String): Toast {
     return Toast(this).also {
         it.duration = Toast.LENGTH_LONG
         it.view = view
+        it.setGravity(Gravity.BOTTOM or Gravity.CENTER_HORIZONTAL, 0,
+                getDimenInt(R.dimen.base_space_8x))
     }
 }
 
